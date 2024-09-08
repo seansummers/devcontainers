980236a8c1d6147858d388cb734d26ad0d08475a
Sean Summers <165854+seansummers@users.noreply.github.com>
2024-09-08 15:51:59 -0400

    added al2023-tenv

---
+++ src/al2023-tenv/install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -xe
+
+trap 'dnf -y clean all' EXIT
+
+dnf -y install \
+  jq
+
+LATEST_COSIGN_VERSION=$(curl https://api.github.com/repos/sigstore/cosign/releases/latest | jq -r .tag_name | tr -d "v")
+
+dnf -y install "https://github.com/sigstore/cosign/releases/latest/download/cosign-${LATEST_COSIGN_VERSION}-1.$(uname -m).rpm" 
+
+LATEST_TENV_VERSION=$(curl https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)
+
+dnf -y install "https://github.com/tofuutils/tenv/releases/download/${LATEST_TENV_VERSION}/tenv_${LATEST_TENV_VERSION}_$(uname -m|sed 's/aarch/arm/').rpm" 
