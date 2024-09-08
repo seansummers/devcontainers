ab60d3165fe890a72c172e5389d1b32e5c40da2b
Sean Summers <165854+seansummers@users.noreply.github.com>
2024-09-08 15:18:49 -0400

    fix typo

--- src/al2023-aws-utils/install.sh
+++ src/al2023-aws-utils/install.sh
@@ -16,7 +16,7 @@ PLATFORM="$(uname -m | sed 's/aarch/arm/')"
 TMP_DIR="$(mktemp -d)"
 trap 'rm -rf "${TMP_DIR}"' EXIT
 cd "${TMP_DIR}"
-curl -LO "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-${PLATFORM}.zip
+curl -LO "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-${PLATFORM}.zip"
 unzip aws-sam-cli-linux-${PLATFORM}.zip
 ./install --update --install-dir /opt/aws-sam-cli
 rm -rf 
