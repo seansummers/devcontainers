6fcc77380ed081171ebc95be103bb5cc14b72a12
Sean Summers <165854+seansummers@users.noreply.github.com>
2024-09-08 20:07:20 -0400

    update typos (again)

--- src/al2023-tenv/install.sh
+++ src/al2023-tenv/install.sh
@@ -6,7 +6,7 @@ dnf -y install \
   jq
 
 ARCH="$(uname -m|sed 's/aarch/arm/;s/x86_/amd/')"
-LATEST_COSIGN_VERSION="$(curl https://api.github.com/repos/sigstore/cosign/releases/latest | jq -r .tag_name | tr -d "v")"
+LATEST_COSIGN_VERSION="$(curl https://api.github.com/repos/sigstore/cosign/releases/latest | jq -r .tag_name | tr -d v)"
 LATEST_TENV_VERSION="$(curl https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)"
 
 dnf -y install "https://github.com/sigstore/cosign/releases/latest/download/cosign-${LATEST_COSIGN_VERSION}-1.$(uname -m).rpm" 
