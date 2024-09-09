a664c36a0165fcd487ee6e2ed9a078d10204bc79
Sean Summers <165854+seansummers@users.noreply.github.com>
2024-09-08 20:13:48 -0400

    fix typos on tenv

--- src/al2023-tenv/install.sh
+++ src/al2023-tenv/install.sh
@@ -10,5 +10,4 @@ LATEST_COSIGN_VERSION="$(curl https://api.github.com/repos/sigstore/cosign/relea
 LATEST_TENV_VERSION="$(curl https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)"
 
 dnf -y install "https://github.com/sigstore/cosign/releases/latest/download/cosign-${LATEST_COSIGN_VERSION}-1.$(uname -m).rpm" 
-
 dnf -y install "https://github.com/tofuutils/tenv/releases/download/${LATEST_TENV_VERSION}/tenv_${LATEST_TENV_VERSION}_${ARCH}.rpm" 
