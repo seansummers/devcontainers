8030fbcc9a8ced5871038b85552514a4630c6432
Sean Summers <165854+seansummers@users.noreply.github.com>
2024-09-08 21:08:46 -0400

    fix /opt/rye permissions

--- src/al2023-rye/install.sh
+++ src/al2023-rye/install.sh
@@ -5,7 +5,11 @@ trap 'dnf -y clean all' EXIT
 RYE_VERSION=${RYEVERSION:-"latest"}
 
 dnf install -y \
+  findutils \
   gzip \
   libxcrypt-compat
 
 curl -sSf https://rye.astral.sh/get | RYE_HOME=/opt/rye RYE_VERSION="${RYE_VERSION}" RYE_INSTALL_OPTION="--yes" bash -xe
+
+chgrp --recursive wheel /opt/rye
+find /opt/rye -type d -exec chmod --recursive ug+rwx {} +
